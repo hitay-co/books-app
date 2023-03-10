86d782a8d34eddc3136fed8c07aef98b458dccfd
hitay-co <hitayco@gmail.com>
2023-03-11 00:06:25 +0300

    get by id api

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import(
 	"net/http"
 	"github.com/gin-gonic/gin"
-	// "errors"
+	 "errors"
 )
 
 type book struct{
@@ -17,6 +17,30 @@ var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+
+func bookById(c* gin.Context){
+	id := c.Param("id")
+	book,err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message" : "Book not found."})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK,book)
+}
+
+// helper
+func getBookById(id string)(*book,error){
+for i, b := range books {
+	if b.ID == id{
+		return &books[i],nil
+	} 
+
+}
+return nil, errors.New("book not found")
 }
 
 func getBooks(c * gin.Context){
@@ -40,6 +64,7 @@ func createBook(c * gin.Context){
 func main(){
 	router := gin.Default()
 	router.GET("/books",getBooks)
+	router.GET("/books/:id",bookById)
 	router.POST("/books",createBook)
 	router.Run("localhost:8080")
 }
